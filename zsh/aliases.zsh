43e95c3590e24e0dc7620aab4f76803edf7d0a4e
Matthew Westrik <m@ttwestrik.com>
2013-12-16 14:30:03 -0500

    initial commit

---
+++ zsh/aliases.zsh
@@ -0,0 +1,7 @@
+alias reload!='. ~/.zshrc'
+alias sasquatch='sass --watch sass:compiled --style compact'
+alias ggo='alias gitgo="git add * && git commit -a && git push origin master"'
+alias ssh-aws='ssh ec2-user@54.213.96.38 -i ~/.ssh/mattw.pem' #good thing i used ssh keys huh otherwise you'd be stealin
+alias cis='cd ~/Academic/CIS*2500/Projects'
+alias _p="cd ~/Projects"
+fliptable() { echo "（╯°□°）╯ ┻━┻"; }
