88570e0cde612f0e9296dd049e42c4a564e54ddb
Matthew Westrik <m@ttwestrik.com>
2019-08-03 21:47:09 -0700

    install ghsync on setup

--- setup.sh
+++ setup.sh
@@ -15,11 +15,19 @@ brew install neovim tmux python@2 python3
 pip3 install --user --upgrade pip setuptools wheel neovim
 pip install --user --upgrade neovim
 
+scripts_folder=$HOME/.local/bin
+mkdir -p $scripts_folder
 
-ln -s $DIR/tmux.conf ~/.tmux.conf
-ln -s $DIR/zshrc ~/.zshrc
-ln -s $DIR/zprezto ~/.zprezto
-ln -s $DIR/zpreztorc ~/.zpreztorc
-ln -s $DIR/gitconfig ~/.gitconfig
-ln -s $DIR/gitignore_global ~/.gitignore_global
-ln -s $DIR/init.vim ~/.config/nvim/init.vim
+# Install script that syncs all GitHub repos to a local folder (set GITHUB_API_TOKEN in ~/.localrc)
+rm -f $scripts_folder/ghsync
+curl https://gist.githubusercontent.com/westrik/2048a98582de72dae0fcee69166a94ee/raw/ > $scripts_folder/ghsync
+chmod +x $scripts_folder/ghsync
+
+ln -s $DIR/tmux.conf $HOME/.tmux.conf
+ln -s $DIR/zshrc $HOME/.zshrc
+rm -rf $HOME/.zprezto
+ln -s $DIR/zprezto $HOME/.zprezto
+ln -s $DIR/zpreztorc $HOME/.zpreztorc
+ln -s $DIR/gitconfig $HOME/.gitconfig
+ln -s $DIR/gitignore_global $HOME/.gitignore_global
+ln -s $DIR/init.vim $HOME/.config/nvim/init.vim
