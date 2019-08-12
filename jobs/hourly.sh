c1db1f716a183ad3ea2e5e698e9d330be371f260
Matthew Westrik <m@ttwestrik.com>
2019-08-11 19:17:49 -0700

    add sounds to terminal notifications

--- jobs/hourly.sh
+++ jobs/hourly.sh
@@ -3,12 +3,15 @@ source $HOME/.dotfiles/env_scripts/folders.sh
 # If there are changes in _/notes, commit them and push.
 # TODO: stash changes, pull, then re-apply
 cd $NOTES_FOLDER
-git diff --quiet && git diff --staged --quiet \
-	|| (git add --all\
-		&& git commit --no-gpg-sign -am '[hourly-job] update with recent changes'\
-		&& git push\
-		&& terminal-notifier\
-			-message "committed + pushed updates in notes" -title "hourly-job")
+(git diff --quiet\
+	&& git diff --staged --quiet\
+	&& terminal-notifier -sound frog -message "no changes in _/notes" -title "hourly-job")\
+|| (git add --all\
+	&& git commit --no-gpg-sign -am '[hourly-job] update with recent changes'\
+	&& git push\
+	&& terminal-notifier\
+		-sound frog -message "committed + pushed updates in _/notes" -title "hourly-job")\
+|| (terminal-notifier -sound sosumi -message "failed to push updates in _/notes" -title "hourly-job")
 
 # Fetch all branches of all of my GitHub repos.
 mkdir -p $LOGS_FOLDER
